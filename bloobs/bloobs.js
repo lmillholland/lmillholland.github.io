52fbbfde5998aa778f759524eddf1bea16a033e4
lewisdas <85946535+lewisdas@users.noreply.github.com>
2021-12-15 18:13:50 -0500

    mobile support
    eh. it's ok. no drag, but it works.

--- bloobs/bloobs.js
+++ bloobs/bloobs.js
@@ -1,5 +1,5 @@
-var BLOOB_SIZE = 35;
-var NUM_RANDOM_BLOOBS = 4000;
+var BLOOB_SIZE = 40;
+var NUM_RANDOM_BLOOBS = 500;
 var IS_MOUSE_DOWN = false;
 
 // ================================================================================================
@@ -15,6 +15,19 @@ function Start()
 	$("body")
 		.mousedown(function() { IS_MOUSE_DOWN = true; })
 		.mouseup(function() { IS_MOUSE_DOWN = false; });
+	
+	$(document).on("touchstart", function() {
+		IS_MOUSE_DOWN = true;
+	});
+	$(document).on("touchmove", function() {
+		console.log("touchmove");
+	});
+	$(document).on("touchend", function() {
+		IS_MOUSE_DOWN = false;
+	});
+	$(document).on("touchcancel", function() {
+		console.log("touchcancel");
+	});
 }
 
 // ================================================================================================
@@ -24,13 +37,13 @@ function SpawnOrderedBloobs()
 {
 	var row = -2;
 	var col = 0;
-	while (row * BLOOB_SIZE < screen.height)
+	while (row * (BLOOB_SIZE - 2) < screen.height)
 	{
 		col = -2;
-		while (col * BLOOB_SIZE < screen.width)
+		while (col * (BLOOB_SIZE - 1) < screen.width)
 		{
 			col++;
-			new Bloob(row * BLOOB_SIZE, col * BLOOB_SIZE);
+			new Bloob(row * BLOOB_SIZE, col * BLOOB_SIZE, "7.5vw");
 		}
 		row++;
 	}
@@ -40,27 +53,40 @@ function SpawnChaoticBloobs()
 {
 	for (var i = 0; i < NUM_RANDOM_BLOOBS; i++)
 	{
-		var top  = (Math.random() * (screen.height + BLOOB_SIZE)) - BLOOB_SIZE;
-		var left = (Math.random() * (screen.width + BLOOB_SIZE)) - BLOOB_SIZE;
-		new Bloob(top, left);
+		var top  = (Math.random() * (screen.height + BLOOB_SIZE * 2)) - BLOOB_SIZE;
+		var left = (Math.random() * (screen.width + BLOOB_SIZE * 2)) - BLOOB_SIZE;
+		var font_size = (Math.random() * 5 + 6) + "vw";
+		new Bloob(top, left, font_size);
 	}
 }
 
 class Bloob {
-	constructor(top, left)
+	constructor(top, left, font_size)
 	{
 		var bloob_html = $("<div class='bloob'></div>");
-		bloob_html.css({"top": top, "left": left});
+		bloob_html.css({
+			"top": top,
+			"left": left,
+			"font-size": font_size
+		});
 		bloob_html.text("🫐");
+
 
 		$("body").append(bloob_html);
 
-		$(bloob_html).bind("mouseover touchstart touchmove", function()
+		$(bloob_html).bind("mouseover touchenter touchstart", function()
 		{
-			if (IS_MOUSE_DOWN)
+			console.log("touching");
+			if (IS_MOUSE_DOWN || true)
 			{
 				$(this).remove();
 			}
+		});
+
+		$(bloob_html).on("touchenter", function()
+		{
+			console.log("touchenter");
+			$(this).remove();
 		});
 	}
 }
@@ -70,6 +96,7 @@ class Bloob {
 
 function ResizeElements()
 {
+	/*
 	$("#message").css(
 	{
 		"height": window.innerHeight + "px",
@@ -79,6 +106,7 @@ function ResizeElements()
 		"height": window.innerHeight / 2 + "px",
 		"line-height": window.innerHeight / 2 + "px",
 	});
+	*/
 }
 
 // ================================================================================================
